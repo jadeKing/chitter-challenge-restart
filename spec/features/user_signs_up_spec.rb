0bc6652e9c7da0bb8df784dba9dc0f40e34740fb
Jade King <dev@jadeking.co.uk>
2015-04-26 12:34:30 +0100

    fix test

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,6 +1,6 @@
 feature 'User signs up' do
   scenario 'when they visit for the first time' do
-    expect { sign_up }.to change(User: count).by(1)
+    expect { sign_up }.to change(User, :count).by(1)
     expect(User.first.email).to eq('test@test.com')
     expect(page).to have_content('Welcome, jd')
   end
